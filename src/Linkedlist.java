ae9b36775bdd5b776ecfd478f2b194b68b57655d
Sanjeev koshti <koshtisanjeev6@gmail.com>
2026-02-23 21:45:41 +0530

    Add import statement for LinkedList class

--- src/Linkedlist.java
+++ src/Linkedlist.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.LinkedList;
+
 public class Linkedlist {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList = new LinkedList<>();
